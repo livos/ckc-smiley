4d33786a2e052d5949bfc51afaa389002c314824
livos <lvivancos2nd-dev@yahoo.fr>
2022-05-08 15:36:45 +0700

    react smiley

--- src/Smiley.js
+++ src/Smiley.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { arc } from "d3";
+
+console.log(arc);
 
 const width = 960;
 const height = 500;
@@ -8,34 +11,36 @@ const strokeWidth = 20;
 const eyeOffsetX = 90;
 const eyeOffsetY = 100;
 const eyeRadius = 40;
+const mouthWidth = 20;
+const mouthRadius = 140;
+
+const mouthArc = arc()
+  .innerRadius(mouthRadius)
+  .outerRadius(mouthRadius + mouthWidth)
+  .startAngle(Math.PI / 2)
+  .endAngle((Math.PI * 3) / 2);
 
 const Smiley = () => {
   return (
     <>
       <div>
         <svg width={width} height={height}>
-          <circle
-            cx={centerX}
-            cy={centerY}
-            r={centerY - strokeWidth / 2}
-            fill="yellow"
-            stroke="black"
-            strokeWidth={strokeWidth}
-          ></circle>
-          <circle
-            cx={centerX - eyeOffsetX}
-            cy={centerY - eyeOffsetY}
-            r={eyeRadius}
-          />
-          <circle
-            cx={centerX + eyeOffsetX}
-            cy={centerY - eyeOffsetY}
-            r={eyeRadius}
-          />
+          <g transform={`translate(${centerX}, ${centerY})`}>
+            <circle
+              r={centerY - strokeWidth / 2}
+              fill="yellow"
+              stroke="black"
+              strokeWidth={strokeWidth}
+            ></circle>
+            <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+            <circle cx={eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+            <path d={mouthArc()} />
+          </g>
         </svg>
       </div>
     </>
   );
+  console.log(mouthArc());
 };
 
 export default Smiley;
